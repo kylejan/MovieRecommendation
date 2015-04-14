933f33bfe8084332e7f0eda72a7e06e62d9b86aa
Y. T. Chung <zonyitoo@gmail.com>
2015-04-14 17:17:15 +0800

    init commit

---
+++ src/edu/hku/comp7305/group1/Recommend.java
@@ -0,0 +1,47 @@
+package edu.hku.comp7305.group1;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.mapred.JobConf;
+
+public class Recommend {
+
+    public static final String HDFS = "hdfs://student3-x1:9000";
+    public static final Pattern DELIMITER = Pattern.compile("[\t,]");
+
+    public static void main(String[] args) throws Exception {
+        Map<String, String> path = new HashMap<String, String>();
+        path.put("data", "logfile/small.csv");
+        path.put("Step1Input", HDFS + "/user/hdfs/recommend");
+        path.put("Step1Output", path.get("Step1Input") + "/step1");
+        path.put("Step2Input", path.get("Step1Output"));
+        path.put("Step2Output", path.get("Step1Input") + "/step2");
+        path.put("Step3Input1", path.get("Step1Output"));
+        path.put("Step3Output1", path.get("Step1Input") + "/step3_1");
+        path.put("Step3Input2", path.get("Step2Output"));
+        path.put("Step3Output2", path.get("Step1Input") + "/step3_2");
+        path.put("Step4Input1", path.get("Step3Output1"));
+        path.put("Step4Input2", path.get("Step3Output2"));
+        path.put("Step4Output", path.get("Step1Input") + "/step4");
+
+        Step1.run(path);
+        Step2.run(path);
+        Step3.run1(path);
+        Step3.run2(path);
+        Step4.run(path);
+        System.exit(0);
+    }
+
+    public static JobConf config() {
+        JobConf conf = new JobConf(Recommend.class);
+        conf.setJobName("Recommend");
+        conf.addResource("classpath:/hadoop/core-site.xml");
+        conf.addResource("classpath:/hadoop/hdfs-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
+        return conf;
+    }
+
+}
