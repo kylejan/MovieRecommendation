5a1d976af088bbff5e519a64248c18240849c240
kylejan <765108938@qq.com>
2015-04-18 11:00:16 +0800

    server update

--- web/movierecommend.py
+++ web/movierecommend.py
@@ -1,12 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for, request
+import happybase
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	# return "xxx"
-    return render_template('index.html')
+        return render_template('index.html', movieList=None)
+
+@app.route('/query', methods=['GET'])
+def query():
+        userID = request.args.get('userID', '')
+
+        conn = happybase.Connection('student3-x2')
+        table = conn.table('MovieRecom')
+        row = table.row(userID)
+
+        movieList = json.loads(row['mr:recommend'])
+
+        return render_template('index.html', movieList=movieList)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
+~
